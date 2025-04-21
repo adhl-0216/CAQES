748723125a7009196c87a90a873a8cc6094f2a8d
Adrian <hanlim2002@gmail.com>
2025-04-21 17:49:06 +0000

    modified syslog-ng test script

--- .devcontainer/config/syslog-ng/generate_snort_logs.sh
+++ .devcontainer/config/syslog-ng/generate_snort_logs.sh
@@ -4,5 +4,5 @@ while true; do
   hostname=$(hostname)
   snort_message="$timestamp $hostname snort[$(($RANDOM % 9000 + 1000))]: [1:$(($RANDOM % 1000000 + 1)):1] Potential Attack [Classification: Attempted Administrator Privilege Gain] [Priority: 1] {TCP} 192.168.$(($RANDOM % 255)).$(($RANDOM % 255)):$(($RANDOM % 65535)) -> 10.0.0.1:80"
   echo "$snort_message" >> /tmp/snort.log
-  sleep 1 # Generate a log message every second
+  sleep 5 # Generate a log message every second
 done
